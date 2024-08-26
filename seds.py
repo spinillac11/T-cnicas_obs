316580b4f8b9bc30c574b91c0224747c844238f1
spinillac11 <92007521+spinillac11@users.noreply.github.com>
2024-08-26 03:55:26 +0000

    se agregó barras de error

--- seds.py
+++ seds.py
@@ -9,29 +9,34 @@ mag = pd.read_csv("./SEDS/merged_data.csv")
 bands_I = ['abs_1_mag_0_I', 'abs_2_mag_0_I', 'abs_3_mag_0_I', 'abs_4_mag_0_I']
 bands_W = ['abs_1_mag_0_W', 'abs_3_mag_0_W', 'abs_4_mag_0_W']
 
-Lambda = np.array([3.6, 4.5, 5.8, 8.0, 3.35, 11.6, 21.1, 23.68])
-#Lambda = np.array([3.6e-4, 4.5e-4, 5.8e-4, 8.0e-4, 3.35e-4, 4.6e-4, 11.6e-4, 21.1e-4, 23.68e-4])
-Fzero = np.array([6.48148e-8, 2.66222e-8, 1.02556e-8, 3.00609e-9, 8.17870e-8, 6.51510e-10, 5.09010e-11, 3.73438e-11])
+bands_I_err = ['abs_1_mag_err_0_I', 'abs_2_mag_err_0_I', 'abs_3_mag_err_0_I', 'abs_4_mag_err_0_I']
+bands_W_err = ['abs_1_mag_err_0_W', 'abs_3_mag_err_0_W', 'abs_4_mag_err_0_W']
 
-#print(mag["abs_2_mag_0_W"])
-# Crear un diccionario para almacenar los flujos calculados
-fluxes = {}
-fluxes["OBJECT_ID"] = mag["OBJECT_ID"]
+Lambda = np.array([3.6, 4.5, 5.8, 8.0, 3.35, 11.6, 21.1, 23.68])
+Fzero = np.array([6.48148e-8, 2.66222e-8, 1.02556e-8, 3.00609e-9, 8.17870e-8, 6.51510e-10, 5.09010e-11, 3.73438e-11])
 
 # Iterar sobre las bandas y calcular los flujos
 for i, band in enumerate(bands_I):
-    fluxes[f'flux_{band}'] = Fzero[i] * 10**((-0.4)*mag[band].to_numpy())
+    mag[f'flux_{band}'] = Fzero[i] * 10**((-0.4)*mag[band].to_numpy())
+    mag[f'flux_{bands_I_err[i]}'] = Fzero[i] * (0.4*np.log(10)) * 10**((-0.4)*mag[band].to_numpy()) * mag[bands_I_err[i]].to_numpy()
 
 for i, band in enumerate(bands_W):
-    fluxes[f'flux_{band}'] = Fzero[i + 4] * 10**((-0.4)*mag[band].to_numpy())
+    mag[f'flux_{band}'] = Fzero[i + 4] * 10**((-0.4)*mag[band].to_numpy())
+    mag[f'flux_{bands_W_err[i]}'] = Fzero[i + 4] * (0.4*np.log(10)) * 10**((-0.4)*mag[band].to_numpy()) * mag[bands_W_err[i]].to_numpy()
 
-fluxes['flux_abs_1_mag_0'] = Fzero[7] * 10**((-0.4)*mag['abs_1_mag_0'].to_numpy())
+mag['flux_abs_1_mag_0'] = Fzero[7] * 10**((-0.4)*mag['abs_1_mag_0'].to_numpy())
+mag[f'flux_abs_1_mag_err_0'] = Fzero[7] * (0.4*np.log(10)) * 10**((-0.4)*mag['abs_1_mag_0'].to_numpy()) * mag['abs_1_mag_err_0'].to_numpy()
+
+################ Errores en el flujo ###############
+
+
+
 
 # Convertir el diccionario a un DataFrame para facilitar la manipulación
-flux_df = pd.DataFrame(fluxes)
+mag_df = pd.DataFrame(mag)
 
 # Crear gráficos para cada estrella
-for index, row in flux_df.iterrows():
+for index, row in mag_df.iterrows():
     plt.figure(figsize=(10, 6))
     
     # Obtener los flujos para la estrella actual
@@ -40,23 +45,29 @@ for index, row in flux_df.iterrows():
     ])
     flux_values = np.append(flux_values, row['flux_abs_1_mag_0'])  # Añadir el flujo del último valor
 
-    # Calcular flujo multiplicado por la longitud de onda
-    flux_lambda = flux_values * Lambda
+    # Error en los flujos
+    flux_values_err = np.array([
+        row[f'flux_{band}'] for band in bands_I_err + bands_W_err
+    ])
+    flux_values_err = np.append(flux_values_err, row['flux_abs_1_mag_0'])
 
     # Ordenar los datos por el eje x
     sorted_indices = np.argsort(Lambda)
     Lambda_sorted = np.array(Lambda)[sorted_indices]
-    flux_lambda_sorted = np.array(flux_lambda)[sorted_indices]
+    flux_sorted = np.array(flux_values)[sorted_indices]
+    flux_err_sorted = np.array(flux_values_err)[sorted_indices]
 
-    print(int(row['OBJECT_ID']), flux_lambda_sorted, "\t")
+    # Calcular flujo multiplicado por la longitud de onda
+    flux_lambda = flux_sorted * Lambda
+
+
+    print(index, int(row['OBJECT_ID']), flux_sorted, "\t")
 
     # Graficar
-    plt.scatter(Lambda_sorted, np.log(flux_lambda_sorted), color='red')
-    plt.plot(Lambda_sorted, np.log(flux_lambda_sorted), linestyle='-', linewidth=2)
+    plt.errorbar(Lambda_sorted, flux_lambda, yerr=flux_err_sorted, fmt='o', ecolor='green', capsize=5, linestyle='-', color='royalblue', markerfacecolor='red', markeredgewidth=0)
     plt.title(f"Flujo vs Longitud de Onda para Estrella ID={int(row['OBJECT_ID'])}")
     plt.xlabel("Log(Longitud de Onda) (µm)")
     plt.ylabel("Log(Flujo x Longitud de Onda) (erg/s/cm²/Hz µm)")
-    plt.xscale('log')
     plt.grid(True)
     plt.savefig(f'./SEDS/seds_{index}.png')
     plt.close()  # Cerrar la figura para liberar memoria
