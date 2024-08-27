abf81f8a6343ffee3b057ff22d862ea7e9496345
spinillac11 <92007521+spinillac11@users.noreply.github.com>
2024-08-27 01:15:32 +0000

    Se agregó gráfica con JHK

--- seds.py
+++ seds.py
@@ -13,7 +13,8 @@ bands_I_err = ['abs_1_mag_err_0_I', 'abs_2_mag_err_0_I', 'abs_3_mag_err_0_I', 'a
 bands_W_err = ['abs_1_mag_err_0_W', 'abs_3_mag_err_0_W', 'abs_4_mag_err_0_W']
 
 Lambda = np.array([3.6, 4.5, 5.8, 8.0, 3.35, 11.6, 21.1, 23.68])
-Fzero = np.array([6.48148e-8, 2.66222e-8, 1.02556e-8, 3.00609e-9, 8.17870e-8, 6.51510e-10, 5.09010e-11, 3.73438e-11])
+###                   I1         I2           I3          I4          W1            W3         W4           M1           J         H        K
+Fzero = np.array([6.48148e-8, 2.66222e-8, 1.02556e-8, 3.00609e-9, 8.17870e-8, 6.51510e-10, 5.09010e-11, 3.73438e-11, 3.129e-6, 1.133e-6, 4.283e-7])
 
 # Iterar sobre las bandas y calcular los flujos
 for i, band in enumerate(bands_I):
@@ -26,10 +27,6 @@ for i, band in enumerate(bands_W):
 
 mag['flux_abs_1_mag_0'] = Fzero[7] * 10**((-0.4)*mag['abs_1_mag_0'].to_numpy())
 mag[f'flux_abs_1_mag_err_0'] = Fzero[7] * (0.4*np.log(10)) * 10**((-0.4)*mag['abs_1_mag_0'].to_numpy()) * mag['abs_1_mag_err_0'].to_numpy()
-
-################ Errores en el flujo ###############
-
-
 
 
 # Convertir el diccionario a un DataFrame para facilitar la manipulación
@@ -57,17 +54,42 @@ for index, row in mag_df.iterrows():
     flux_sorted = np.array(flux_values)[sorted_indices]
     flux_err_sorted = np.array(flux_values_err)[sorted_indices]
 
-    # Calcular flujo multiplicado por la longitud de onda
-    flux_lambda = flux_sorted * Lambda
 
-
-    print(index, int(row['OBJECT_ID']), flux_sorted, "\t")
+    print(index, int(row['OBJECT_ID']), flux_values, "\t")
 
     # Graficar
-    plt.errorbar(Lambda_sorted, flux_lambda, yerr=flux_err_sorted, fmt='o', ecolor='green', capsize=5, linestyle='-', color='royalblue', markerfacecolor='red', markeredgewidth=0)
+    plt.errorbar(np.log10(Lambda_sorted), np.log10(flux_sorted * Lambda_sorted), yerr=flux_err_sorted/(np.log(10)*flux_sorted), fmt='o', ecolor='green', capsize=5, linestyle='-', color='royalblue', markerfacecolor='red', markeredgewidth=0)
     plt.title(f"Flujo vs Longitud de Onda para Estrella ID={int(row['OBJECT_ID'])}")
     plt.xlabel("Log(Longitud de Onda) (µm)")
     plt.ylabel("Log(Flujo x Longitud de Onda) (erg/s/cm²/Hz µm)")
     plt.grid(True)
     plt.savefig(f'./SEDS/seds_{index}.png')
     plt.close()  # Cerrar la figura para liberar memoria
+
+    if int(row['OBJECT_ID']) == 3336110521410475904:
+        plt.figure(figsize=(10, 6))
+        
+        magJ =  12.329 - 5 * np.log10(row['distance_pc']) + 5 - 3.1 * 0.103 * 0.28665
+        magH =  11.658 - 5 * np.log10(row['distance_pc']) + 5 - 3.1 * 0.103 * 0.18082
+        magK =  11.327 - 5 * np.log10(row['distance_pc']) + 5 - 3.1 * 0.103 * 0.11675
+        fluxJ = Fzero[8] * 10**((-0.4)*magJ)
+        fluxH = Fzero[9] * 10**((-0.4)*magH)
+        fluxK = Fzero[10] * 10**((-0.4)*magK)
+
+        Lambda_sorted = np.insert(Lambda_sorted, 0, 2.159)
+        Lambda_sorted = np.insert(Lambda_sorted, 0, 1.662)
+        Lambda_sorted = np.insert(Lambda_sorted, 0, 1.235)
+
+        flux_sorted = np.insert(flux_sorted, 0, fluxK)
+        flux_sorted = np.insert(flux_sorted, 0, fluxH)
+        flux_sorted = np.insert(flux_sorted, 0, fluxJ)
+
+        # Graficar
+        plt.plot(np.log10(Lambda_sorted), np.log10(Lambda_sorted*flux_sorted))
+        plt.title(f"Flujo vs Longitud de Onda para Estrella ID={int(row['OBJECT_ID'])}")
+        plt.xlabel("Log(Longitud de Onda) (µm)")
+        plt.ylabel("Log(Flujo x Longitud de Onda) (erg/s/cm²/Hz µm)")
+        plt.grid(True)
+        plt.savefig(f'./SEDS/seds_{index}_JHK.png')
+        plt.close()  # Cerrar la figura para liberar memoria
+        
